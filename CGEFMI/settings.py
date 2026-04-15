3ea626524903ebd583b7f87e85e633d270aca2a8
Rehoboth Ajose <ajoserehoboth049@gmail.com>
2026-04-15 08:44:02 +0100

    Fix syntax error in ALLOWED_HOSTS list

--- CGEFMI/settings.py
+++ CGEFMI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = [['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
