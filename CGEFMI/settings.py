90660ab8e980ebf49cb95355e471aa8b6745f177
Rehoboth Ajose <ajoserehoboth049@gmail.com>
2026-04-15 08:31:05 +0100

    Update SECRET_KEY to use a placeholder
    Replaced the hardcoded SECRET_KEY with a placeholder.

--- CGEFMI/settings.py
+++ CGEFMI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7jt2hks+6!*v5fnpyxj--6on8z%6&n4*$_m4t4sh6qkue@_!@)'
+SECRET_KEY = ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
