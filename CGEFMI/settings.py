c288558da8e6c901bd394bad59e5a96019140027
Rehoboth Ajose <ajoserehoboth049@gmail.com>
2026-05-03 00:47:19 +0100

    Update settings.py

--- CGEFMI/settings.py
+++ CGEFMI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +80,11 @@ WSGI_APPLICATION = 'CGEFMI.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('POSTGRES_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -138,7 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 JAZZMIN_SETTINGS = {
     'site_header': 'CGEFMI',
     'site_brand' : 'CGEFMI',
-    'site_logo' : 'asset/logo.png',
     "welcome_sign": "Built by Rebix",
 }
 
